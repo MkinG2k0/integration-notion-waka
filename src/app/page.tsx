08f7c3d75ebbcb92916ab221d50d7beb5fc9aae4
kamai <kamai1220000@gmail.com>
2023-07-12 00:15:56 +0300

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,13 @@ import { NotionAuthBtn } from 'shared'
 import { NextPage } from 'next'
 
 const Page = async () => {
+	const data = await prisma.user.findMany()
+
+	return (
+		<div>
+			{data.length} <NotionAuthBtn />
+		</div>
+	)
 	return (
 		<div className={'col justify-center align-middle  '}>
 			<div className={'flex justify-center p-2'}>
--- src/shared/config/env.ts
+++ src/shared/config/env.ts
@@ -5,4 +5,3 @@ export const BASE_API = BASE_URL.concat('api/')
 export const CLIENT_ID = process.env.NEXT_OAUTH_CLIENT_ID
 export const CLIENT_SECRET = process.env.NEXT_OAUTH_CLIENT_SECRET
 export const REDIRECT_URI = process.env.NEXT_OAUTH_REDIRECT_URI
-console.log('ok')
