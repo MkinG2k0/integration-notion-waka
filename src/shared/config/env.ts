90822c4d0fa5078525a505d77c323b5baf1f6238
kamai <kamai1220000@gmail.com>
2023-07-11 19:16:46 +0300

    fix

--- src/shared/config/env.ts
+++ src/shared/config/env.ts
@@ -5,3 +5,11 @@ export const BASE_API = BASE_URL.concat('api/')
 export const CLIENT_ID = process.env.NEXT_OAUTH_CLIENT_ID
 export const CLIENT_SECRET = process.env.NEXT_OAUTH_CLIENT_SECRET
 export const REDIRECT_URI = process.env.NEXT_OAUTH_REDIRECT_URI
+
+console.log(DOMAIN, 'DOMAIN')
+console.log(IS_LOCAL, 'IS_LOCAL')
+console.log(BASE_URL, 'BASE_URL')
+console.log(BASE_API, 'BASE_API')
+console.log(CLIENT_ID, 'CLIENT_ID')
+console.log(CLIENT_SECRET, 'CLIENT_SECRET')
+console.log(REDIRECT_URI, 'REDIRECT_URI')
