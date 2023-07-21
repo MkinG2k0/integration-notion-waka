67b0e358aa1674aa1d2ae26e47a262c1de4eecaf
kamai <kamai1220000@gmail.com>
2023-07-22 01:31:20 +0300

    refactor

--- src/app/api/test/route.ts
+++
@@ -1,8 +0,0 @@
-import { NextResponse } from 'next/server'
-import { redis } from 'server/auth/redis'
-
-export const GET = async () => {
-	const data = await redis.get('user:account:notion:*')
-
-	return NextResponse.json({ data })
-}
